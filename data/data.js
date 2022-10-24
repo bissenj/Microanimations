f33f69be98596e2ec878508fbba6c7224faccb3f
bissenj <bissenj@gmail.com>
2022-10-24 09:49:58 -0600

    Styling work timeline

---
+++ data/data.js
@@ -0,0 +1,19 @@
+
+
+// Data
+const events = [
+    {
+        yearStart: 1998, 
+        yearEnd:2003, 
+        name: 'University of Wisconsin - Platteville', 
+        tools: '.NET, JAVA, Visual Basic', 
+        image: 'uwp.webp',
+        summary: 'Bachelors in Computer Science, Emphasis on Software Engineering.',
+        story: 'I graduated with a degree in Computer Science and a focus on Software Engineering.  I felt the need to prove myself at that age so I spent my time taking the hardest software, math and science classes I could, the ones geared for engineers.  I learned how to work in groups and that sometimes you need to step up and lead the group in order to get things done.  At one point I also had 3 W2 jobs and a paying gig job as the drummer in a cover band.  I didn’t want to be in debt.  If I could go back in time and redo the whole experience, I would make myself slow down and relax a bit.  On the upside I came away with my future wife, 8 close friends I still hang out with to this day, several job offers, and learned to snowboard.'
+    },
+    {yearStart: 2000, yearEnd:2006, name: "Wisconsin Air National Guard", tools: "HTML, CSS, Javascript", description: "Computer support and a bit of web development in the Air National Guard"},
+    {yearStart: 2004, yearEnd:2014, name: "Northwestern Mutual", tools: "Java, .Net, Message Queues, Middleware", description: "Worked in a number of roles and teams during my 10 years at Northwestern Mutual."}, 
+    {yearStart: 2014, yearEnd:2021, name: "NAH", tools: "Web Design, Marketing, Planning, Leading", description: "Owned small business"}, 
+    {yearStart: 2018, yearEnd:2021, name: "M.J. Electric", tools:"Javascript, .Net, React, Adobe XD", description: "Worked on the web development team."}, 
+    {yearStart: 2021, yearEnd:2023, name: "Self Employed", tools: "React, Remix, Figma", description: "Taking a break to live life."}, 
+]
